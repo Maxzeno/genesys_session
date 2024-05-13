ac3a856652ba52d99e5b5caa5881f3267f790281
Emmanuel Nwaegunwa <mmnlchidera@gmail.com>
2024-05-13 18:49:38 +0100

    js basics

---
+++ main.js
@@ -0,0 +1,50 @@
+var name = "Emmanuel" // string (text)
+
+// name = "David"
+
+// console.log(name)
+
+let a = 5 // int 
+let b = 6
+
+// adding a and b to c
+let c = a + b
+
+console.log(c)
+
+
+let pi = 3.142 // float or double 
+console.log(pi)
+
+
+// = is a assignment operator
+// while == and === is a comparism operator
+
+
+console.log(1 == 2)
+
+let isRainy = true;
+
+
+if (isRainy == true) {
+    console.log("Don't go out")
+} else {
+    console.log("Go out")
+}
+
+let age = 3000;
+let message = "";
+
+if (age < 10) {
+    console.log("i am less than 10")
+    message = "less than 10"
+} else if (age == 10) {
+    console.log("I am 10")
+    message = "exactly 10"
+} else {
+    console.log("I am greater than 10")
+    message = "greater than 10"
+}
+
+
+console.log(message)
